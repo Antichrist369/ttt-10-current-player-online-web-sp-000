a03bb87cba0e771aac078a2d60437c7682835cfd
Anthony Molina <antonio-molina@att.net>
2019-06-04 03:43:36 +0000

    Automatically backed up by Learn

---
+++ lib/current_player.rb
@@ -0,0 +1,13 @@
+def turn_count(turn)
+  counter = l 
+  move_count = []
+  turn.each do |unit|
+    counter = l 
+    if unit == "X" || unit == "O"
+      puts "spot free"
+      counter += l 
+    end 
+  end 
+  move_count.length.i 
+end 
+
